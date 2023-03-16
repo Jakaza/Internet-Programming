6cec645b2af002847375865f019239dd93061d33
Themba G <goodnessthembac@gmail.com>
2023-03-16 09:21:05 +0200

    SubjectManager to determine if student qualifies and map the servlet in web xml

---
+++ development/Admission Point Score/src/java/za/ac/tut/controller/SubjectSevlet.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package za.ac.tut.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import za.ac.tut.model.SubjectManager;
+
+/**
+ *
+ * @author Jakaza G Chauke
+ */
+public class SubjectSevlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        
+        Integer sub1 = Integer.parseInt(request.getParameter("subject1-mark"));
+        Integer sub2 = Integer.parseInt(request.getParameter("subject2-mark"));
+        Integer sub3 = Integer.parseInt(request.getParameter("subject3-mark"));
+        Integer sub4 = Integer.parseInt(request.getParameter("subject4-mark"));
+        Integer sub5 = Integer.parseInt(request.getParameter("subject5-mark"));
+        
+        Integer[] marks = {sub1,sub2, sub3, sub4 , sub5}; 
+        
+        SubjectManager subjectManager = new SubjectManager();
+        Integer aps = subjectManager.calculateAPS(marks);
+        String outcome = subjectManager.getOutcome(aps);
+        
+        request.setAttribute("aps", aps);
+        request.setAttribute("outcome", outcome);
+        
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("output.jsp");
+    }
+}
---
+++ development/Admission Point Score/src/java/za/ac/tut/model/SubjectManager.java
@@ -0,0 +1,58 @@
+package za.ac.tut.model;
+
+/**
+ *
+ * @author Jakaza G Chauke
+ */
+public class SubjectManager {
+
+    public SubjectManager() {
+    }
+
+    public Integer calculateAPS(Integer[] marks) {
+        Integer aps = 0;
+        for(int i = 0; i < marks.length; i++){
+            if(marks[i] > 7){
+                if(marks[i] > 7 && marks[i] <= 29)
+                    aps += 1;
+                else if(marks[i] >= 30 && marks[i] <= 39)
+                    aps += 2;
+                else if(marks[i] >= 40 && marks[i] <= 49)
+                    aps += 3;
+                else if(marks[i] >= 50 && marks[i] <= 59)
+                    aps += 4;
+                else if(marks[i] >= 60 && marks[i] <= 69)
+                    aps += 5;
+                else if(marks[i] >= 70 && marks[i] <= 79)
+                    aps += 6;
+                else if(marks[i] >= 80 && marks[i] <= 100)
+                    aps += 7;
+                else{
+                    // Invalid Input
+                }
+            }
+            if(marks[i] >= 0 && marks[i] <= 7){
+             aps += marks[i];   
+            }else{
+                // Invalid Input
+            }  
+        }
+        return aps;
+    }
+
+    public String getOutcome(Integer aps , String major) {
+        String outcome = "Do not meet requirements.";
+        if((major.equals("mathematics") && aps >= 26)
+                || (major.equals("lit") && aps >= 28))
+        {
+           outcome += "Meet requirements for mainstream."; 
+        }
+        else if((major.equals("mathematics") && aps >= 23)
+                    ||(major.equals("lit") && aps >= 25))
+        {
+           outcome += "Meet requirements for extended program."; 
+        }        
+        return outcome;
+    }
+    
+}
